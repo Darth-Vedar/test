8b6ac7fc91041afa6d16e6919f6be403be569eb3
Darth-Vedar <robinsinghshekhawat6@gmail.com>
2020-08-01 13:18:09 +0530

    second

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ROBIN n jhhjnj
+                    REX
                 </div>
 
                 <div class="links">
