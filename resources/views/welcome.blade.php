1ba86a3ca3b03d23c45e0a972b9fda0e450399a7
Darth-Vedar <robinsinghshekhawat6@gmail.com>
2020-08-01 13:13:52 +0530

    fisrt commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    REX
+                    ROBIN
                 </div>
 
                 <div class="links">
